f16496f29228ed34a79efac190a51bea9e2dbd10
manuel rivero <manuel.enrique.r.v@gmail.com>
2019-12-07 23:45:04 -0300

    [CSS]: adding fade in animation to images

--- src/UI/imgSpinner/imgSpinner.js
+++ src/UI/imgSpinner/imgSpinner.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import spinnerGif from '../../assets/images/spinner.gif';
 import styles from './imgSpinner.module.css';
 
-export default function imgSpinner(props) {
+export default (props)=>{
+    const [imageStatus, setImageStatus] = useState(false)
     const {largeImageURL, tags}=props;
+    const loadHandler = ()=>{
+        setImageStatus(true);
+    }
+
       return (
     <>
         <div className={styles.ImgContainer}style={{backgroundImage:`url(${spinnerGif})`}}>
-              <img  src={largeImageURL} alt={tags} style={{width:'100%'}} />
+              <img  src={largeImageURL} 
+                    alt={tags} 
+                    className={imageStatus && styles.FadeIn}
+                    style={{width:'100%',backgroundColor: '#fff'}} 
+                    onLoad={loadHandler}/>
         </div>
     </>
   );
