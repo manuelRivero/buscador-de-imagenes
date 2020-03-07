45b87b0b1b578301d45e197ab0b8948462d7c764
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-03-06 22:36:45 -0300

    [fix] modal buttons actions

--- src/componentes/imgModal/imgModal.js
+++ src/componentes/imgModal/imgModal.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Modal, Button} from 'react-bootstrap';
-import styles from './imgModal.module.css'
 
 import ImgSpinner from './../../UI/imgSpinner/imgSpinner';
 
@@ -13,17 +12,14 @@ export default function ImgModal(props) {
   const {largeImageURL, tags}=props.data
     
   return (
-    <Modal show={props.show} onHide={props.close} >
+    <Modal show={props.show} onHide={props.close}  centered >
    
     <Modal.Body>
         <ImgSpinner largeImageURL={largeImageURL} tags={tags} />
     </Modal.Body>
     <Modal.Footer>
       <Button variant="secondary" onClick={props.close}>
-        Close
-      </Button>
-      <Button variant="primary" onClick={props.close}>
-        Save Changes
+        Cerrar
       </Button>
     </Modal.Footer>
   </Modal>
